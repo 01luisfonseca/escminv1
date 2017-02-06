807af23b73fc3f228328a5edbdf3877c2cc5555a
Luis Andres Fonseca Gomez <Luis Andres Fonseca Gomez>
2017-02-05 21:42:23 -0500

    Inicialización del sistema

---
+++ app/Asignaciones.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asignaciones extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'asignaciones';
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'U';
+    public $timestamps = false;
+
+    public function discursos(){
+        return $this->hasMany('App\discursos');
+    }
+}
---
+++ app/Discursos.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discursos extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'discursos';
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'U';
+    public $timestamps = false;
+
+    public function asignaciones(){
+        return $this->belongsTo('App\Asignaciones');
+    }
+
+    public function estudiantes(){
+        return $this->belongsTo('App\Estudiantes');
+    }
+}
